3ffa5f7834e69386db57ef198b37bc899c47e273
Kayla Phelps <kmphelps822@gmail.com>
2021-08-18 11:07:24 -0700

    submission for deadline

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushi, setSushi] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then((data) => setSushi(data))
+  }, [])
+
+
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushi={sushi} setSushi={setSushi}  />
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,27 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, setSushi, nextFour }) {
+
+  
+  const sushiComponents = sushi.filter(sush => sush.id < 5).map((sush) => (
+      <Sushi 
+      key={sush.id}
+      name={sush.name}
+      img_url={sush.img_url}
+      price={sush.price}
+      created_at={sush.created_at}
+      />
+  ));
+
+
+  
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton nextfour={sushiComponents}/>
     </div>
   );
 }
